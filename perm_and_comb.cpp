5b1e6fb3d33c69b1d965e6d39c5862193502f3b8
DeepanshuAttri <deepanshuattri55@gmail.com>
2020-01-25 05:04:47 +0530

    second commit

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -3,15 +3,66 @@ using namespace std;
 
 int permutation(int n, int r)
 {
+	 if(r==0)
+  {
+     return 1;
+  }
+  else
+  {
+  	return (n)*permutation(n-1,r-1);
+  }
+  
   return 0;
 }
 
 int combination(int n, int r)
 {
+	 
+  if(r==0)
+  {
+     return 1;
+  }
+  else
+  {
+  	return (n)*combination(n-1,r-1)/r;
+  }
+  
   return 0;
 }
 
 int main()
 {
+		int x=1;
+	float per,com,n,r;
+	cout<<"enter the number you want to do"<<endl;
+	cin>>n>>r;
+	try
+	{
+		if (r==0)
+		{
+			per=1;
+			com=1;
+		}
+		else if (n<0||r<0)
+		{
+			throw x;
+		}
+		else if(n<r)
+		{
+			throw x;
+		}
+		else
+		{
+			per=permutation(n,r);
+			com=combination(n,r);
+			
+		}
+		cout<<"\n permutation is:: "<<per;
+		cout<<"\n combination is:: "<<com;
+	}
+	catch(int a)
+	{
+		cout<<"\n do not expected";
+	}
   return 0;
 }
